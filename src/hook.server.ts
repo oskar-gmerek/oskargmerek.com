ba15ea18cae263dffc111e9f9abb82c61c759367
Oskar Gmerek <oskarg90@gmail.com>
2023-04-18 17:34:47 +0100

    trivial: remove misnamed hooks file

--- src/hook.server.ts
+++
@@ -1,9 +0,0 @@
-import { renderIntoHTML } from '@master/css';
-import { config } from '../master.css';
-import type { Handle } from '@sveltejs/kit';
-
-export async function handle({ event, resolve }) {
-	return (await resolve(event, {
-		transformPageChunk: ({ html }) => renderIntoHTML(html, config)
-	})) satisfies Handle;
-}
