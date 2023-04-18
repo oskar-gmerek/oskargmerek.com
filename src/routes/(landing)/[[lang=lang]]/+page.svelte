4cec2eb3ecee27cef1215d02388cfbdecf9308a6
Oskar Gmerek <oskarg90@gmail.com>
2023-04-18 23:10:58 +0100

    `trivial`: remove deprecated files

---
+++ src/routes/(landing)/[[lang=lang]]/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { T, getTranslate } from '@tolgee/svelte';
+
+	const { t } = getTranslate();
+</script>
+
+<h1>Hi there! <br />I'm Oskar Gmerek.</h1>
+<p>This is my personal website.</p>
+
+<!-- <T keyName="key" defaultValue="This is default" />
+
+OR
+
+{$t('this_is_a_key')} -->
