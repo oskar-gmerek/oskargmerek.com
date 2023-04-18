d53ae4971686b4a9da799a45d646aa4bfaccb83c
Oskar Gmerek <oskarg90@gmail.com>
2023-04-18 22:47:31 +0100

    closes #1;`i18n`: add `tolgee` integration

---
+++ src/lib/helpers/locales.ts
@@ -0,0 +1,10 @@
+export const languages = {
+	en: '🇬🇧 English',
+	pl: '🇵🇱 Polish',
+	'en-GB': 'Britain'
+	// cs: '🇨🇿 česky',
+	// fr: '🇫🇷 français',
+	// de: '🇩🇪 Deutsch'
+};
+
+export const defaultLang = 'en';
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,40 @@
-<script>
+<script lang="ts">
 	import { LazyCSSProvider } from '@master/css.svelte';
+	import '@master/normal.css';
+	import {
+		TolgeeProvider,
+		Tolgee,
+		DevTools,
+		FormatSimple,
+		LanguageStorage,
+		LanguageDetector
+	} from '@tolgee/svelte';
+	import { languages, defaultLang } from '$lib/helpers/locales';
+	import type { LayoutData } from './$types';
+
+	export let data: LayoutData;
+
 	const loadConfig = import('../master.css');
+	const tolgee = Tolgee()
+		.use(DevTools())
+		.use(FormatSimple())
+		.use(LanguageStorage())
+		.use(LanguageDetector())
+		.init({
+			language: data.lang ? data.lang : defaultLang,
+			staticData: {
+				[data.lang]: data.langData
+			},
+			defaultLanguage: data.lang ? data.lang : defaultLang,
+			availableLanguages: Object.keys(languages),
+			apiUrl: import.meta.env.VITE_TOLGEE_API_URL,
+			apiKey: import.meta.env.VITE_TOLGEE_API_KEY
+		});
 </script>
 
 <LazyCSSProvider config={loadConfig}>
-	<div class="app"><slot /></div>
+	<TolgeeProvider {tolgee}>
+		<div slot="fallback" class="m:auto fg:orange-70">Loading translations...</div>
+		<slot />
+	</TolgeeProvider>
 </LazyCSSProvider>
