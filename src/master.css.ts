a3f96cc17dc2ce5248d078517a2d17414a65a3b8
Oskar Gmerek <oskarg90@gmail.com>
2023-04-20 04:25:34 +0100

    `css`: add custom colors to `masterCSS`

--- src/master.css.ts
+++ src/master.css.ts
@@ -1,3 +1,11 @@
 import type { Config } from '@master/css';
+import { rules } from '@master/css';
 
-export const config: Config = {};
+export const config: Config = {
+	colors: {
+		danger: '#ff0000', // class="bg:danger fill:danger"
+		warn: '#ffbf00', // class="fg:warn outline:warn"
+		orange: 'rgb(225,122,32)',
+		background: 'rgb(23,33,45)'
+	}
+};
