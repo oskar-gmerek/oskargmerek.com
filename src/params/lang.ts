f5fd8ef456980ec4d4d6fb2d095cf5b67a2fc3de
Oskar Gmerek <oskarg90@gmail.com>
2023-04-20 04:24:49 +0100

    `i18n`: add url matcher for `i18n`

--- src/params/lang.ts
+++ src/params/lang.ts
@@ -1,8 +1,6 @@
 import type { ParamMatcher } from '@sveltejs/kit';
-import { languages } from '$lib/helpers/locales';
 
 export const match = ((param) => {
-	// const regex = new RegExp('([' + Object.keys(languages).join('|') + ']){2|5}');
 	const regex = new RegExp('([a-z]{2}(-[a-zA-Z]{2})?)');
 	return regex.test(param);
 }) satisfies ParamMatcher;
