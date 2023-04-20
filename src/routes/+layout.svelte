e6d640ab76c98fa7c626ff1510b6644b37ec1e21
Oskar Gmerek <oskarg90@gmail.com>
2023-04-20 04:34:47 +0100

    `feat`: integrate `Open Sans` and `Ubuntu` fonts closes #14

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,7 +9,7 @@
 		LanguageStorage,
 		LanguageDetector
 	} from '@tolgee/svelte';
-	import { languages, defaultLang } from '$lib/helpers/locales';
+	import { languages, defaultLang } from '$lib/i18n/locales';
 	import type { LayoutData } from './$types';
 
 	export let data: LayoutData;
@@ -35,6 +35,55 @@
 <LazyCSSProvider config={loadConfig}>
 	<TolgeeProvider {tolgee}>
 		<div slot="fallback" class="m:auto fg:orange-70">Loading translations...</div>
-		<slot />
+		<div class="fg:white">
+			<slot />
+		</div>
 	</TolgeeProvider>
 </LazyCSSProvider>
+
+<style>
+	@font-face {
+		font-family: 'Ubuntu';
+		src: local('Ubuntu-Bold'), local('UbuntuBold'), local('Ubuntu Bold'),
+			local('Ubuntu-VariableFont'), local('Ubuntu-Variable') url('/fonts/ubuntu/Ubuntu-Bold.woff2');
+		src: url('/fonts/ubuntu/Ubuntu-Bold.woff2');
+		font-weight: 700;
+		font-display: optional;
+	}
+	@font-face {
+		font-family: 'Open Sans';
+		src: local('OpenSans-Regular'), local('OpenSans'), local('Open-Sans'), local('OpenSans Regular'),
+			local('Open Sans Regular'), local('OpenSansRegular'), local('OpenSans-VariableFont_wdth,wght'),
+			local('OpenSans-VariableFont'), url('/fonts/opensans/OpenSans-Regular.woff2');
+		src: url('/fonts/opensans/OpenSans-Regular.woff2');
+		font-weight: 400;
+		font-display: optional;
+	}
+	@font-face {
+		font-family: 'Open Sans-fallback';
+		size-adjust: 105.40000000000003%;
+		ascent-override: 107%;
+		src: local('Arial');
+		font-display: optional;
+		font-weight: 400;
+	}
+	@font-face {
+		font-family: 'Ubuntu-fallback';
+		size-adjust: 102.19999999999999%;
+		ascent-override: 87%;
+		src: local('Arial');
+		font-display: optional;
+		font-weight: 700;
+	}
+	:global(html) {
+		background-color: rgb(32, 44, 59);
+	}
+	:global(h1, h2, h3, h4, h5, h6) {
+		font-family: 'Ubuntu', 'Ubuntu-fallback', sans-serif;
+		font-weight: 700;
+	}
+	:global(body:not(h1, h2, h3, h4, h5, h6)) {
+		font-family: 'Open Sans', 'Open Sans-fallback', sans-serif;
+		font-weight: 400;
+	}
+</style>
