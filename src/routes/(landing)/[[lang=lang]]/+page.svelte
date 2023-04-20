06933ea6df84e502b9b237fce1e8dd853d0691cb
Oskar Gmerek <oskarg90@gmail.com>
2023-04-20 04:35:23 +0100

    `feat`: add simple introduction paragraph

--- src/routes/(landing)/[[lang=lang]]/+page.svelte
+++ src/routes/(landing)/[[lang=lang]]/+page.svelte
@@ -4,8 +4,14 @@
 	const { t } = getTranslate();
 </script>
 
-<h1>Hi there! <br />I'm Oskar Gmerek.</h1>
-<p>This is my personal website.</p>
+<h1>Hello, world! <span class="">I'm Oskar,</span></h1>
+<p>
+	an aspiring web developer on a mission to conquer the digital landscape. Fueled by curiosity and a
+	hunger for growth, I am constantly exploring new technologies and techniques to sharpen my skills.
+	While my portfolio may be modest for now, my determination and passion speak volumes. Stick
+	around, witness my journey, and let's connect – we might just collaborate on something
+	extraordinary!
+</p>
 
 <!-- <T keyName="key" defaultValue="This is default" />
 
