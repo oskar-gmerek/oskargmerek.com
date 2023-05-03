4954a55b708c090e36d77ca19572764c1cf06fa7
Oskar Gmerek <oskarg90@gmail.com>
2023-05-03 01:29:17 +0100

    `deps`: `imagetools` integration for image optimization closes #24

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import { imagetools } from 'vite-imagetools';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), imagetools()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
