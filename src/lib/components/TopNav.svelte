4e4b111526fc0d9c4e9976560d961975a5198378
Oskar Gmerek <oskarg90@gmail.com>
2023-05-03 02:07:32 +0100

    `feat`: top navigation closes #6 closes #7

--- src/lib/components/TopNav.svelte
+++ src/lib/components/TopNav.svelte
@@ -3,16 +3,30 @@
 	import LangSelect from '$lib/components/LangSelect.svelte';
 </script>
 
-<div class="fixed top:0 w:full">
+<div class="fixed top:0 w:full z:100">
 	<nav class="flex m:auto max-w:lg bg:background ai:center py:10">
 		<Logo class="h:70 p:10 fill:orange b:green|2|2" />
 		<div class=""><LangSelect /></div>
-		<div class="flex p:5>div m:5>div m:20@sm>div bg:orange>div:hover">
-			<div class="">Home</div>
-			<div class="">Projects</div>
-			<div class="hidden block@2xs">Articles</div>
-			<div class="hidden block@2xs">Notes</div>
-			<div class="">Contact</div>
+		<div class="flex p:5>div m:5>div m:20@sm>div">
+			<div class="bg:orange:hover"><a href="/">Home</a></div>
+			<div class="bg:gray/.2:hover fg:gray/.7 cursor:not-allowed" title="Work In Progress">
+				Projects
+			</div>
+			<div
+				class="bg:gray/.2:hover fg:gray/.7 cursor:not-allowed hidden block@2xs"
+				title="Work In Progress"
+			>
+				Articles
+			</div>
+			<div
+				class="bg:gray/.2:hover fg:gray/.7 cursor:not-allowed hidden block@2xs"
+				title="Work In Progress"
+			>
+				Notes
+			</div>
+			<div class="bg:gray/.2:hover fg:gray/.7 cursor:not-allowed" title="Work In Progress">
+				Contact
+			</div>
 		</div>
 	</nav>
 </div>
