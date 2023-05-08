68660d43e68b96ba92838cb245b6a8ab5186554e
Oskar Gmerek <oskarg90@gmail.com>
2023-05-08 01:05:08 +0100

    feat: initial `Select` component

---
+++ src/lib/components/select/Select.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import randomString from '$lib/utils/randomString';
+	export let value = '';
+	export let name = '';
+	export { className as class };
+	export let label = '';
+	export let description = '';
+	export let containerStyle = '';
+	type Options =
+		| {
+				label: string;
+				value: string;
+		  }[]
+		| null;
+	export let options: Options = null;
+	let className =
+		'w:full h:full p:16 pr:30 outline:4|solid|rgb(8,19,33) outline:4|solid|orange:focus ~outline|0.5s r:7  appearance:none z:10';
+	export let mcss = '';
+
+	const id = randomString(7) + name;
+</script>
+
+{#if label}
+	<label for={id}>{label}</label>
+{/if}
+{#if description}
+	<div class="fg:white/.6">Description of input</div>
+{/if}
+<div class={` bg:rgb(8,19,33) flex align-items:center mt:10 mb:30 ` + ' ' + containerStyle}>
+	<select {id} class={className + ' ' + mcss} {name} bind:value on:change>
+		{#if options}
+			{#each options as option}
+				<option value={option.value}>{option.label}</option>
+			{/each}
+		{:else}
+			<slot name="options" />
+		{/if}
+	</select>
+	<div class="rel">
+		<div class="abs top:-7 left:-20 f:12">⋁</div>
+	</div>
+</div>
