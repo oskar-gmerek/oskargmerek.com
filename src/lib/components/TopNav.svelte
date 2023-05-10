6e03f8c6d03df22b0052628069ffe6aa99612875
Oskar Gmerek <oskarg90@gmail.com>
2023-05-11 00:35:48 +0100

    improve: make `logo` as `link` to homepage

--- src/lib/components/TopNav.svelte
+++ src/lib/components/TopNav.svelte
@@ -5,7 +5,9 @@
 
 <div class="fixed top:0 w:full z:100">
 	<nav class="flex m:auto max-w:lg bg:background ai:center py:10">
-		<Logo class="h:70 p:10 fill:orange b:green|2|2" />
+		<a href="/" title="web developer portfolio"
+			><Logo class="h:70 p:10 fill:orange b:green|2|2" /></a
+		>
 		<div class=""><LangSelect /></div>
 		<div class="flex p:5>div m:5>div m:20@sm>div">
 			<div class="bg:orange:hover"><a href="/">Home</a></div>
