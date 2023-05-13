637d9515b41be3329e8c9d1e08a78ebf7f307baa
Oskar Gmerek <oskarg90@gmail.com>
2023-05-13 23:35:32 +0100

    improve: types for `ContactForm`

---
+++ src/lib/types/contact.d.ts
@@ -0,0 +1,34 @@
+export type ContactForm = {
+	emailSubject: string;
+	emailMessage: string;
+	website: string;
+	subjectOptions: string;
+
+	deadlineType?: string;
+	projectName?: string;
+	projectDetails?: string;
+	budget?: string;
+	additionalRequirements?: string;
+	deadline?: string;
+	otherContact?: string;
+	companyName?: string;
+	workPosition?: string;
+	jobDescription?: string;
+	workMode?: string;
+	workLocation?: string;
+	salaryOffer?: string;
+	additionalInformation?: string;
+	naturePartnership?: string;
+	expectedBenefits?: string;
+	organisationType?: string;
+
+	organisationName?: string;
+	collaborationDetails?: string;
+	expectedOutcomes?: string;
+	investmentDetails?: string;
+	expectedReturns?: string;
+
+	fullName: string;
+	emailAddress?: string;
+	mobileNumber?: string;
+};
