8e6ad865bf12a03d2622037256959f801b83245c
Oskar Gmerek <oskarg90@gmail.com>
2023-05-16 23:13:51 +0100

    fix: check if `languages` include `lang` before replace in html
    closes #51

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,10 +2,10 @@ import type { Handle } from '@sveltejs/kit';
 import { renderIntoHTML } from '@master/css';
 import { config } from './master.css';
 import { sequence } from '@sveltejs/kit/hooks';
-import { defaultLang } from '$lib/i18n/locales';
+import { defaultLang, languages } from '$lib/i18n/locales';
 
 const handleLang = (async ({ event, resolve }) => {
-	console.log('pathname.length', event.url.pathname.split('/').length);
+	const langArray = Object.entries(languages).map(([label]) => label);
 	const [, lang, ...rest] = event.url.pathname.split('/');
 	if (lang === defaultLang && event.url.pathname !== '/') {
 		// If default language is in the url path, remove it
@@ -16,7 +16,7 @@ const handleLang = (async ({ event, resolve }) => {
 	}
 
 	return resolve(event, {
-		transformPageChunk: ({ html }) => html.replace('%lang%', lang || 'en')
+		transformPageChunk: ({ html }) => html.replace('%lang%', langArray.includes(lang) ? lang : 'en')
 	});
 }) satisfies Handle;
 
