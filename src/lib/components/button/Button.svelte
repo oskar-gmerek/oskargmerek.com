35301fc001abec5f3c4ffca5415f6b0fd96df089
Oskar Gmerek <oskarg90@gmail.com>
2023-05-17 21:02:01 +0100

    improve: detect `href` prop to render `a` tag

--- src/lib/components/button/Button.svelte
+++ src/lib/components/button/Button.svelte
@@ -3,13 +3,16 @@
 	export let href = '';
 	export let hreflang = '';
 	export let title = '';
+	export let mcss = '';
 	export { className as class };
 
 	let className =
-		'fg:#17212d font:bold bg:orange  py:10 px:12 r:7 outline:2|solid|#17212d outline:2|solid|orange:hover b:2|solid|#17212d';
+		'fg:#17212d font:bold bg:orange ml:5 py:10 px:12 r:7 outline:2|solid|#17212d outline:2|solid|orange:hover b:2|solid|#17212d' +
+		' ' +
+		mcss;
 </script>
 
-{#if type === 'a'}
+{#if type === 'a' || href !== ''}
 	<a {href} {hreflang} class={className} {title}><slot>Go to</slot></a>
 {:else}
 	<button {type} class={className} {title} on:click><slot>Submit</slot></button>
